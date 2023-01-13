eb0578058abe049d552d4d48fde6313f7f636ad3
evg1kr <113228938+evg1kr@users.noreply.github.com>
2023-01-13 22:26:36 +0300

    Add files via upload

---
+++ Visitor/src/visitors/Visitor.java
@@ -0,0 +1,13 @@
+package visitors;
+
+import shapes.*;
+
+public interface Visitor {
+    String visitDot(Dot dot);
+
+    String visitCircle(Circle circle);
+
+    String visitRectangle(Rectangle rectangle);
+
+    String visitCompoundGraphic(CompoundShape cg);
+}
